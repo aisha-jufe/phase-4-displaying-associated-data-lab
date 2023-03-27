453eb126b78b1684dbf20638556e421c02d45dce
aisha-jufe <aishajucfe287>
2023-03-27 10:53:34 +0300

    completed

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,2 +1,17 @@
 class ItemsController < ApplicationController
-end
+    def index
+      items = Item.all.map { |item| {
+        id: item.id,
+        name: item.name,
+        description: item.description,
+        price: item.price,
+        user: {
+          id: item.user.id,
+          username: item.user.username,
+          city: item.user.city
+        }
+      }}
+      render json: items
+    end
+  end
+  
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,17 @@
 class UsersController < ApplicationController
-end
+    def show
+      user = User.find(params[:id])
+      render json: {
+        id: user.id,
+        username: user.username,
+        city: user.city,
+        items: user.items.map { |item| {
+          id: item.id,
+          name: item.name,
+          description: item.description,
+          price: item.price
+        }}
+      }
+    end
+  end
+  
